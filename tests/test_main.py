ad5c0d82652bbbc734baa519c6f5fa2e5410baef
Priyabrata Panda <priyabrata409@gmail.com>
2021-10-28 19:35:43 +0530

    Second commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,9 @@ from src.main import calculate_median,calculate_mode
 
 def test_calculate_median():
     assert calculate_median([1,2,3,4,5])==3
+    assert calculate_median([1,2,3,4,5,6])==3.5
 
 def test_calculate_mode():
     assert calculate_mode([1,2,3,3,4,5])==3
+    assert calculate_mode([1,2,3,3,5,5,5,4,5])==5
 
